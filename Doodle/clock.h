1394a86cfdaa39e048fa811625bf7ebb704f216b
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-12-25 20:50:48 -0500

    Added comments

--- Doodle/clock.h
+++ Doodle/clock.h
@@ -7,9 +7,16 @@ namespace doodle {
 
 	class Clock final {
 	public:
+
+		// Starts/Restarts the clock.
 		void start() noexcept;
+		
+		// Returns the time elapsed since creation or the last call to start()
 		std::uint32_t elapsed() const noexcept;
+
+		// Returns time in between calls.
 		std::uint32_t ticks() noexcept;
+
 	private:
 		std::uint32_t cur = SDL_GetTicks();
 	};
