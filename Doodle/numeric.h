291ee9909a4c62526069149852e40747c155f463
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-12-28 14:44:09 -0500

    Added comment

--- Doodle/numeric.h
+++ Doodle/numeric.h
@@ -4,6 +4,7 @@
 
 namespace doodle {
 
+	// Checks if two floating point values are approximately equal.
 	template <class T> std::enable_if_t<std::is_floating_point_v<T>, bool> is_equal(T a, T b) noexcept;
 
 }
