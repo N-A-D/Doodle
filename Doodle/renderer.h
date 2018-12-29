e42a1ab11c7c29b4848ac5d4b54c628b96a0e6a2
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-12-28 21:01:55 -0500

    Added type safety for template members

--- Doodle/renderer.h
+++ Doodle/renderer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+#include <type_traits>
 #include "texture.h"
 #include "sdl_resource.h"
 
@@ -34,29 +35,33 @@ namespace doodle {
 		void draw_point(const SDL_Point& point) const noexcept;
 
 		// Draws a series of points onto this renderer's parent window.
-		template <class Container>
-		void draw_points(const Container& points) const noexcept;
+		template <template<class> class Container, class T>
+		std::enable_if_t<std::is_same_v<SDL_Point, T>, void> 
+			draw_points(const Container<T>& points) const noexcept;
 
 		// Draws a line onto the renderer's parent window.
 		void draw_line(const std::pair<SDL_Point, SDL_Point>& line) const noexcept;
 
 		// Draws multiple lines onto the renderer's parent window.
-		template <class Container>
-		void draw_lines(const Container& lines) const noexcept;
+		template <template<class> class Container, class T>
+		std::enable_if_t<std::is_same_v<std::pair<SDL_Point, SDL_Point>, T>, void>
+			draw_lines(const Container<T>& lines) const noexcept;
 
 		// Draws a rectangle outline onto the renderer's parent window.
 		void draw_rect(const SDL_Rect& rect) const noexcept;
 
 		// Draws multiple rectangle outlines onto the renderer's parent window.
-		template <class Container>
-		void draw_rects(const Container& rects) const noexcept;
+		template <template<class> class Container, class T>
+		std::enable_if_t<std::is_same_v<SDL_Rect, T>, void>
+			draw_rects(const Container<T>& rects) const noexcept;
 
 		// Draws a filled rectangle onto the renderer's parent window.
 		void draw_filled_rect(const SDL_Rect& filled_rect) const noexcept;
 
 		// Draws multiple filled rectangles onto the renderer's parent window.
-		template <class Container>
-		void draw_filled_rects(const Container& filled_rects) const noexcept;
+		template <template<class> class Container, class T>
+		std::enable_if_t<std::is_same_v<SDL_Rect, T>, void> 
+			draw_filled_rects(const Container<T>& filled_rects) const noexcept;
 
 		bool operator==(const Renderer& other) const noexcept;
 		bool operator!=(const Renderer& other) const noexcept;
@@ -75,29 +80,33 @@ namespace doodle {
 	// Loads a new SDL_Renderer resource from a doodle::Window.
 	std::pair<SDL_Renderer*, std::uint32_t> create_renderer(Window& window, std::uint32_t flags);
 
-	template<class Container>
-	inline void Renderer::draw_points(const Container & points) const noexcept
+	template<template<class> class Container, class T>
+	inline std::enable_if_t<std::is_same_v<SDL_Point, T>, void> 
+		Renderer::draw_points(const Container<T> & points) const noexcept
 	{
 		for (auto point : points)
 			draw_point(point);
 	}
 
-	template<class Container>
-	inline void Renderer::draw_lines(const Container & lines) const noexcept
+	template<template<class> class Container, class T>
+	inline std::enable_if_t<std::is_same_v<std::pair<SDL_Point, SDL_Point>, T>, void> 
+		Renderer::draw_lines(const Container<T> & lines) const noexcept
 	{
 		for (auto line : lines)
 			draw_line(line);
 	}
 
-	template<class Container>
-	inline void Renderer::draw_rects(const Container & rects) const noexcept
+	template <template<class> class Container, class T>
+	inline std::enable_if_t<std::is_same_v<SDL_Rect, T>, void> 
+		Renderer::draw_rects(const  Container<T> & rects) const noexcept
 	{
 		for (auto rect : rects)
 			draw_rect(rect);
 	}
 
-	template<class Container>
-	inline void Renderer::draw_filled_rects(const Container & filled_rects) const noexcept
+	template <template<class> class Container, class T>
+	inline std::enable_if_t<std::is_same_v<SDL_Rect, T>, void>
+		Renderer::draw_filled_rects(const Container<T> & filled_rects) const noexcept
 	{
 		for (auto filled_rect : filled_rects)
 			draw_filled_rect(filled_rect);
