9d0152ffa76100c665ff61280a8179f3efb03b5c
Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-12-30 15:09:22 -0500

    Reduced particle count

--- Particles/main.cpp
+++ Particles/main.cpp
@@ -8,9 +8,9 @@
 #include <random>
 #include <sstream>
 
-const int NUM_RECTS = 30'000;
+const int NUM_RECTS = 200;
 
-void create_rects(rift::EntityManager& entities, int num_entities) noexcept;
+void create_particles(rift::EntityManager& entities, int num_entities) noexcept;
 
 int main(int argc, char** argv) {
 	doodle::init();
@@ -30,7 +30,7 @@ int main(int argc, char** argv) {
 	systems.add<MovementSystem>();
 	systems.add<RenderingSystem>(renderer);
 
-	create_rects(entities, NUM_RECTS);
+	create_particles(entities, NUM_RECTS);
 
 	while (window.poll_events()) {
 		if (handler.is_key_pressed(doodle::Keyboard::KEY_ESCAPE))
@@ -38,7 +38,7 @@ int main(int argc, char** argv) {
 		time_text.str("");
 		time_text << fc.avg_frame_rate() << " FPS";
 		window.set_title(time_text.str());
-		renderer.clear({ 255, 255, 255, 255 });
+		renderer.clear({ 0, 0, 0, 0 });
 		systems.update<MovementSystem, RenderingSystem>(clock.delta_time() / 1000.0);
 		renderer.display();
 	}
@@ -46,19 +46,19 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-void create_rects(rift::EntityManager & entities, int num_entities) noexcept
+void  create_particles(rift::EntityManager & entities, int num_entities) noexcept
 {
 	std::random_device rd;
 	std::mt19937 gen(rd());
 	std::uniform_real_distribution<float> angle_dis(0.f, 360.f);
-	std::uniform_real_distribution<float> speed_dis(100.f, 500.f);
+	std::uniform_real_distribution<float> speed_dis(100.f, 300.f);
 
 	for (int i = 0; i < num_entities; i++)
 	{
 		auto e = entities.create_entity();
-		e.add<Body>(3, 3, doodle::Vector2f(400, 300), 
+		e.add<Body>(5, 5, doodle::Vector2f(400, 300), 
 			doodle::rotate(doodle::Vector2f(speed_dis(gen), 0), angle_dis(gen)));
-		SDL_Color color = { 255, 0, 0, 255 };
+		SDL_Color color = { 255, 255, 255, 255 };
 		e.add<Render>(color);
 	}
 }
