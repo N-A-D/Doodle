81e841b9cdaaf4d5d7221916a6c2f9c343f454da
Austin Datiles <ned.austin.datiles@ryerson.ca>
2019-01-31 20:02:53 -0500

    include'd all relevant headers

--- Doodle/doodle.h
+++ Doodle/doodle.h
@@ -1,5 +1,18 @@
 #pragma once
 
+#include "asset/asset.h"
+#include "audio/music.h"
+#include "audio/sound.h"
+#include "display/window.h"
+#include "display/renderer.h"
+#include "graphics/texture.h"
+#include "graphics/font.h"
+#include "system/clock.h"
+#include "system/fps.h"
+#include "system/input.h"
+#include "system/keyboard.h"
+#include "system/mouse.h"
+
 namespace doodle {
 
 	// Initializes Doodle.
